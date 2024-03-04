aed2de4d3f408d5e65a22857e5ca467dac5b2230
ruchaliulistech <144226272+ruchaliulistech@users.noreply.github.com>
2024-03-04 16:57:27 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,40 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import Welcome from './screens/Welcome';
+import Login from './screens/Login';
+import Signup from './screens/Signup';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName='Welcome'
+      >
+        <Stack.Screen
+          name="Welcome"
+          component={Welcome}
+          options={{
+            headerShown: false
+          }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{
+            headerShown: false
+          }}
+        />
+        <Stack.Screen
+          name="Signup"
+          component={Signup}
+          options={{
+            headerShown: false
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ screens/Index.js
@@ -0,0 +1,9 @@
+import Login from "./Login";
+import Welcome from "./Welcome";
+import Signup from "./Signup";
+
+export default {
+    Login,
+    Welcome,
+    Signup
+}
